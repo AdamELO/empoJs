8b8be2c6cad42da4b9fd637d87e08e8bb3187249
Adam <elomari-adam@hotmail.com>
2020-02-13 09:47:19 +0100

    scroll nav fixed Ok

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 // btn black or white
+let nav
 let body = document.querySelector("body");
 let white = document.getElementsByTagName("button")[1];
 let black = document.getElementsByTagName("button")[2];
@@ -6,6 +7,7 @@ let black = document.getElementsByTagName("button")[2];
 white.addEventListener("click",()=>{
     body.style.backgroundColor="white";
     document.getElementsByTagName("h1")[0].style.color="black";
+    document.getElementsByTagName("h1")[1].style.color="black";
     for (let i = 0; i < 6; i++) {
         document.getElementsByTagName("a")[i].style.color="grey";
     }
@@ -21,10 +23,13 @@ white.addEventListener("click",()=>{
     for (let i = 0; i < 6; i++) {
         document.getElementsByClassName("borderwhite")[i].style.border="solid 10px white";
     }
+    nav.classList.remove("bg-black");
+    nav.classList.add("bg-white");
 })
 black.addEventListener("click",()=>{
     body.style.backgroundColor="black";
     document.getElementsByTagName("h1")[0].style.color="white";
+    document.getElementsByTagName("h1")[1].style.color="white";
     for (let i = 0; i < 6; i++) {
         document.getElementsByTagName("a")[i].style.color="white";
     }
@@ -40,6 +45,29 @@ black.addEventListener("click",()=>{
     for (let i = 0; i < 6; i++) {
         document.getElementsByClassName("borderwhite")[i].style.border="solid 10px black";
     }
+    nav.classList.remove("bg-white");
+    nav.classList.add("bg-black");
 })
 
+// scroll fixed navabar
+nav = document.getElementsByTagName("nav")[0];
+let header = document.getElementsByTagName("header")[0];
+
+function scrolled(){
+    var currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    if(currentScroll >= header.clientHeight){
+    nav.classList.add("fixed-top");        
+    header.classList.add("d-none");      
+    document.getElementById("emporium").classList.remove("d-none");
+    document.getElementsByClassName("jumbotron")[0].style.marginTop = "5%";
+
+    }else{
+        nav.classList.remove("fixed-top");        
+        header.classList.remove("d-none");      
+        document.getElementById("emporium").classList.add("d-none");
+        document.getElementsByClassName("jumbotron")[0].style.marginTop = "0";
+    }
+  }
+  
+addEventListener("scroll", scrolled);
 
